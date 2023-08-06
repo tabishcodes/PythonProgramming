04860741640f09894d79ffe19f602a44baf8a281
TabishCodes <tabishsheikhcs@gmail.com>
2023-08-06 20:34:07 +0530

    August6

--- LearningAug5.py
+++ LearningAug5.py
@@ -26,8 +26,8 @@ print(lin1.partition("apple")) #it will return the whole string in 1st tuple if 
 print("The banana & banana is red & yellow.".replace("banana", "apple", 1)) #the value in the last decides how many times value will be changes
 print(lin1.rfind("banana")) #finds the value and returns the pointer place else return -1
 print(lin1.rfind("banana", 6, 10)) #finds the value b/w specific range and returns the pointer place else return -1
-print(lin1.rindex("apple")) #finds the value and returns the pointer place else return error
-print(lin1.rindex("banana", 5, 10)) #finds the value b/w specific range and returns the pointer place else return error
+print(lin1.rindex("banana")) #finds the value and returns the pointer place else return error
+print(lin1.rindex("banana", 0, 10)) #finds the value b/w specific range and returns the pointer place else return error
 print(lin1.split()) #will split the string where there is space in b/w i.e. after every word
 print(lin1.split("a", 1)) #split the string by replacincing the specific char and only for give times
 print("The banana \n is yellow.".splitlines()) #splits a string into a list ; splitting is done at line breaks
@@ -36,3 +36,49 @@ print(lin1.startswith("The", 6, 10)) #check the char between specific positions
 print(lin1.swapcase()) #return opposite cases of every char in the string
 mydict = {83:   80} ; txt2="Steel" ; print(txt2.translate(mydict))
 print("110".zfill(10)) #fill the starting pointers with 0 then at last display the value.
+
+#ESCAPING CHARACTERS
+print("Bra\"ein") # \ (backslash) will make the next char does'nt effect the coming char.
+print("Bra\nein") # \n means new line
+print("Bra\\ein") #this is used to use a \ in between
+
+#STRING CHARACTERS & SLICING
+print(name[0]) #this will display the index value as followed.
+print(name[1:5]) #this will display the index value between the following value.
+
+#BOOLEANS
+done = True #in numerics +ve & -ve values ; in string any char in double quotes are considered true but 0;empty str is considered as false. 
+
+if done:
+    print("Yes")
+else:
+    print("No")\
+    
+book_1_read = True
+book_2_read = False
+
+read_any_book = any([book_1_read, book_2_read]) #as it says any if anyone is true then rest is also true.
+
+print(read_any_book)
+
+ingredients_purchased = True
+meal_cooked = False
+
+ready_to_serve = all([ingredients_purchased, meal_cooked])
+
+print(ready_to_serve) #as it says all means if all the values are true then its true else false
+
+#NUMBER DATA TYPES
+num1 = 2+3j
+num2 = complex(2,3)
+
+print(num2.real,num2.imag)
+
+#BUILT-IN-FUNCTION
+print(abs(-5.5)) #it make every value +ve
+print(round(5.49)) #it round-off (if its below 0.50 then it's 0 & if it's equal to or more then 0.50 the its 1)
+print(round(5.49, 1)) #now in the second parameter we can set at what decimal point we need to round off.
+
+#ENUMS -readable names that are bounded to constant values.
+
+
