b8c2096321319f0a66bf8c6ac571fea5a81e37a5
TabishCodes <tabishsheikhcs@gmail.com>
2023-08-22 22:32:06 +0530

    August21

---
+++ LearningAug21.py
@@ -0,0 +1,51 @@
+#BLACKJACK {with card classes}
+
+#BLACKJACK
+import random
+
+class Card:
+    def __init__(self):
+        self.suits = "hearts"
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+        suits = ["spades", "clubs", "hearts", "diamonds"]
+        ranks = [{"rank": "A" , "value": 11 },
+                {"rank": "2" , "value": 2 },
+                {"rank": "3" , "value": 3 },
+                {"rank": "4" , "value": 4 },
+                {"rank": "5" , "value": 5 },
+                {"rank": "6" , "value": 6 },
+                {"rank": "7" , "value": 7 },
+                {"rank": "8" , "value": 8 },
+                {"rank": "9" , "value": 9 },
+                {"rank": "10" , "value": 10 },
+                {"rank": "J" , "value": 10 },
+                {"rank": "Q" , "value": 10 },
+                {"rank": "K" , "value": 10 },]
+
+        for suit in suits:
+            for rank in ranks:
+                self.cards.append([suit, rank])
+
+    def shuffle(self):
+        if len(self.cards) > 1 :
+            random.shuffle(self.cards)
+
+    def deal(self, number):
+        cards_dealt = []
+        for x in range(number):
+            if len(self.cards) > 0 :
+                card = self.cards.pop()
+                cards_dealt.append(card)
+        return cards_dealt 
+
+deck1 = Deck()
+deck1.shuffle()
+print(deck1.cards)
+
+deck2 = Deck()
+deck2 = Deck()
+print(deck2.cards)
+
